d8929a653e2d3b5bf4774d43d4b557aa40d9a55d
Vittorio Adesso <adessovittorio8@gmail.com>
2021-11-01 20:56:56 +0100

    Iteración #3

---
+++ modElDOm3.js
@@ -0,0 +1,14 @@
+/* 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array. */
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+
+/* 2.7 Elimina todos los nodos que tengan la clase.fn - remove - me */
+
+/* 2.8 Inserta una p con el texto 'Voy en medio!'
+entre los dos div.
+Recuerda que no solo puedes insertar elementos con.appendChild.  */
+
+/* 2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase.fn - insert - here */
+
+
+
+// ITERACIÓN 3
